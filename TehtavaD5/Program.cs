b72622899e56841af4490805f702e1986c0eea41
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-22 05:51:11 +0300

    Add project files.

---
+++ TehtavaD5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static string GreetUser(string name)
+        {
+            // Luo tervehdysviesti käyttäjän nimellä
+            string greeting = $"Hei {name}";
+            return greeting;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä nimesi: ");
+            string userName = Console.ReadLine();
+
+            // Luo tervehdysviesti ja tulosta se
+            string greetingMessage = GreetUser(userName);
+            Console.WriteLine(greetingMessage);
+        }
+    }
+}
+
